1311dedba248cb1a6e91db3b007995a27435029d
VictorHSCosta <victorhenriquecosta23@gmail.com>
2026-07-07 21:54:45 -0300

    refactor: update default output directory in GeneratePipeline3Verilog to 'generated'

--- src/main/scala/riscv/pipeline/GeneratePipeline3Verilog.scala
+++ src/main/scala/riscv/pipeline/GeneratePipeline3Verilog.scala
@@ -5,7 +5,7 @@ import java.nio.file.Paths
 
 object GeneratePipeline3Verilog extends App {
   val programFile = args.headOption.getOrElse("modelsim/program.hex")
-  val outputDir = args.drop(1).headOption.getOrElse("assembly_code/generated")
+  val outputDir = args.drop(1).headOption.getOrElse("generated")
   val absoluteProgramFile = Paths.get(programFile).toAbsolutePath.normalize.toString
 
   (new ChiselStage).emitVerilog(
